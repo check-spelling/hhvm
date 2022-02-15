20f88d3c3b088cd0d70acba58cbbe5e711d505eb
Josh Soref <jsoref@users.noreply.github.com>
2022-02-14 22:43:22 -0500

    spelling: visibility
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hphp/hack/src/parser/core/declaration_parser.rs
+++ hphp/hack/src/parser/core/declaration_parser.rs
@@ -1628,7 +1628,7 @@ where
         //
         // variadic-parameter:
         //   ...
-        //   attribute-specification-opt visiblity-modifier-opt type-specifier \
+        //   attribute-specification-opt visibility-modifier-opt type-specifier \
         //     ...  variable-name
         //
         // This function parses the parens as well.
