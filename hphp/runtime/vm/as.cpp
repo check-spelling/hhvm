06ab249771630b302afc3e9eef5e816ba6aa60d4
Josh Soref <jsoref@users.noreply.github.com>
2022-02-14 22:43:12 -0500

    spelling: identifier
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hphp/runtime/vm/as.cpp
+++ hphp/runtime/vm/as.cpp
@@ -3048,8 +3048,8 @@ void parse_enum_ty(AsmState& as) {
 }
 
 /*
- * directive-require : 'extends' '<' indentifier '>' ';'
- *                   | 'implements' '<' indentifier '>' ';'
+ * directive-require : 'extends' '<' identifier '>' ';'
+ *                   | 'implements' '<' identifier '>' ';'
  *                   ;
  *
  */
