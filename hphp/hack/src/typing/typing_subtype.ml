bf8783ac60182790cf8cb9da0b4fe02463f96dd8
Josh Soref <jsoref@users.noreply.github.com>
2022-02-14 22:43:12 -0500

    spelling: heuristically
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hphp/hack/src/typing/typing_subtype.ml
+++ hphp/hack/src/typing/typing_subtype.ml
@@ -1250,7 +1250,7 @@ and simplify_subtype_i
                 let ty_sub = LoclType ty_sub in
                 res ||| simplify_subtype_i ~subtype_env ~this_ty ty_sub ty_super)
           in
-          (* Heuristicky logic to decide whether to "break" the intersection
+          (* Heuristically logic to decide whether to "break" the intersection
              or the union first, based on observing that the following cases often occur:
                - A & B <: (A & B) | C
                  In which case we want to "break" the union on the right first
