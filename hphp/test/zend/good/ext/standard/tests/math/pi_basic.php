6e623caa6e3802b0b8bcf6778f4ed3c9bfc90cd4
Josh Soref <jsoref@users.noreply.github.com>
2022-02-14 22:43:20 -0500

    spelling: specified
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hphp/test/zend/good/ext/standard/tests/math/pi_basic.php
+++ hphp/test/zend/good/ext/standard/tests/math/pi_basic.php
@@ -2,5 +2,5 @@
 echo pi(), "\n";
 echo M_PI, "\n";
 // N.B pi() ignores all specified arguments no error
-// messages are produced if arguments are spcified.
+// messages are produced if arguments are specified.
 }
