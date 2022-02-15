9a7e8bc33ec61bbd020105c6f7263cddbe7d6bf2
Josh Soref <jsoref@users.noreply.github.com>
2022-02-14 22:43:13 -0500

    spelling: initialize
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hphp/runtime/base/vanilla-dict.cpp
+++ hphp/runtime/base/vanilla-dict.cpp
@@ -115,7 +115,7 @@ ArrayData* VanillaDict::MakeReserveDict(uint32_t size) {
   auto const scale = computeScaleFromSize(size);
   auto const ad    = reqAlloc(scale);
 
-  // Intialize the hash table first, because the header is already in L1 cache,
+  // Initialize the hash table first, because the header is already in L1 cache,
   // but the hash table may not be.  So let's issue the cache request ASAP.
   ad->initHash(scale);
 
