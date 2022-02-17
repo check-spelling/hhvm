4055ff25a93fff3092d92f8acab8872399a37875
Josh Soref <jsoref@users.noreply.github.com>
2022-02-17 00:46:36 -0500

    spelling: synthesized
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- hphp/hack/src/typing/typing_extends.ml
+++ hphp/hack/src/typing/typing_extends.ml
@@ -1408,7 +1408,7 @@ let check_class_extends_parent
 
 (** Eliminate all synthesized members (those from requirements) plus
 all private members unless they're from traits. *)
-let filter_privates_and_synthethized
+let filter_privates_and_synthesized
     ~(is_trait : bool) (members : ('a * class_elt) list) : ('a * class_elt) list
     =
   let eliminate class_elt =
@@ -1426,7 +1426,7 @@ let make_parent_member_map
   |> MemberKindMap.of_list
   |> MemberKindMap.map (fun members ->
          members
-         |> filter_privates_and_synthethized
+         |> filter_privates_and_synthesized
               ~is_trait:(Ast_defs.is_c_trait (Cls.kind parent_class))
          |> SMap.of_list
          |> SMap.map (fun member ->
